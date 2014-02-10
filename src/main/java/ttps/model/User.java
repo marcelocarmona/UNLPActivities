06f518be6c2b8e4adbb16b55c7736c9e4d6ceb7e
mclo <mclo0202@gmail.com>
2014-02-10 15:53:46 -0300

    agegue username unico para spring data

--- src/main/java/ttps/model/User.java
+++ src/main/java/ttps/model/User.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -35,6 +36,7 @@ public class User implements UserDetails, Serializable{
 	
 	
 	/* Spring Security fields */
+	@Column(unique=true)
 	private String username;
 	private String password;
 	@OneToMany(cascade = CascadeType.PERSIST,fetch = FetchType.EAGER)
